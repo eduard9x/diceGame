3a68b882e8a9d4d3b5a905ee4b2a8edef595398d
eduard9x <ghinea.eduard@yahoo.com>
2015-12-06 13:23:41 +0000

    created the IntelliJ Project

---
+++ src/myPackage/Main.java
@@ -0,0 +1,11 @@
+package myPackage;
+
+/**
+ * Created by Sophie on 06/12/15.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hell");
+    }
+}
